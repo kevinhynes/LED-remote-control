dbcc24758bad00ce6afaa4b112dc6a16f6b2979a
kevin <khynes57@outlook.com>
2022-12-09 11:53:34 -0500

    First commit, skeleton app started

---
+++ main.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.config import Config
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.colorpicker import ColorPicker
+
+Config.set('graphics', 'width', 325)
+Config.set('graphics', 'height', 600)
+
+
+from jnius import autoclass
+# BluetoothAdapter = autoclass('android.bluetooth.BluetoothAdapter')
+
+
+
+class CornerLightMenu(BoxLayout):
+    pass
+
+
+class CornerLightApp(App):
+    def build(self):
+        return CornerLightMenu()
+
+
+if __name__ == "__main__":
+    CornerLightApp().run()
