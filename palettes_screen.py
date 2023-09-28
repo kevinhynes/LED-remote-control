e4b2df33848251e3229d3d5edae8114fe2234dce
fifteen <fifteen@AndroidAppVM15.myguest.virtualbox.org>
2023-09-27 20:12:36 -0400

    Changing up palettes interface

--- palettes_screen.py
+++ palettes_screen.py
@@ -2,9 +2,12 @@ import logging
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.relativelayout import MDRelativeLayout
-from kivy.properties import NumericProperty, ListProperty, ObjectProperty
+from kivy.properties import NumericProperty, ListProperty, ObjectProperty, BooleanProperty
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.card import MDCard
+from kivymd.uix.gridlayout import MDGridLayout
+from kivymd.uix.selectioncontrol import MDCheckbox
 from kivy.uix.screenmanager import SlideTransition
 
 from kivy.graphics import Color
@@ -20,39 +23,68 @@ from troubleshooting import func_name
 
 
 ########## Palettes Screen ##########
-class GradientWidget(MDBoxLayout):
+class PaletteWidgetControls(MDGridLayout):
+    pass
+
+class GradientWidget(MDFlatButton):
+    mirrored = BooleanProperty(True)
 
     def __init__(self, colors, **kwargs):
         super().__init__(**kwargs)
         # Right now only works for hex values in string form
-        colors = [str(color) for color in colors]
+        self.colors = [str(color) for color in colors]
+        self.mirrored_colors = self.colors + self.colors[::-1]
         self.gradient = RoundedRectangle()
         gradient_texture = Gradient.horizontal(*[get_color_from_hex(color) for color in colors])
         with self.canvas:
             Color(1, 1, 1)  # Set color to white, not sure why this is necessary.
             self.gradient = RoundedRectangle(size=self.size, pos=self.pos,
-                                             radius=(dp(20), dp(20)), texture=gradient_texture)
+                                             radius=(dp(15), dp(15)), texture=gradient_texture)
             self.bind(pos=self.update_gradient, size=self.update_gradient)
 
     def update_gradient(self, *args):
         self.gradient.pos = self.pos
         self.gradient.size = self.size
 
+    def on_mirrored(self, *args):
+        logging.debug(f'`{self.__class__.__name__}.{func_name()}`')
+        self.canvas.clear()
+        if self.mirrored:
+            gradient_texture = Gradient.horizontal(
+                *[get_color_from_hex(color) for color in self.mirrored_colors])
+            with self.canvas:
+                Color(1, 1, 1)  # Set color to white, not sure why this is necessary.
+                self.gradient = RoundedRectangle(size=self.size, pos=self.pos,
+                                                 radius=(dp(15), dp(15)), texture=gradient_texture)
+                self.bind(pos=self.update_gradient, size=self.update_gradient)
+        else:
+            gradient_texture = Gradient.horizontal(
+                *[get_color_from_hex(color) for color in self.colors])
+            with self.canvas:
+                Color(1, 1, 1)  # Set color to white, not sure why this is necessary.
+                self.gradient = RoundedRectangle(size=self.size, pos=self.pos,
+                                                 radius=(dp(15), dp(15)), texture=gradient_texture)
+                self.bind(pos=self.update_gradient, size=self.update_gradient)
 
-class PaletteWidget(MDRelativeLayout):
+    def change_mirror(self, *args):
+        logging.debug(f'`{self.__class__.__name__}.{func_name()}`called with {args}')
+        self.mirrored = not self.mirrored
+
+
+class PaletteWidget(MDCard):
 
     def __init__(self, device_controller, hex_colors, **kwargs):
         super().__init__(**kwargs)
         self.device_controller = device_controller
         self.hex_colors = hex_colors
         self.gradient_widget = GradientWidget(hex_colors)
-        self.button = MDFlatButton(pos_hint={'center_x': 0.5, 'center_y': 0.5},
-                                   size_hint=(1, 1))
-        self.button.bind(on_press=self.send_palette)
+        self.gradient_widget.bind(on_press=self.send_palette)
+        self.controls = PaletteWidgetControls()
         self.add_widget(self.gradient_widget)
-        self.add_widget(self.button)
+        self.add_widget(self.controls)
 
     def send_palette(self, *args):
+        logging.debug(f'`{self.__class__.__name__}.{func_name()}`')
         command = Command(mode=3, hex_colors=self.hex_colors)
         self.device_controller.send_command(command)
 
@@ -91,4 +123,3 @@ class PalettesScreen(MDScreen):
         app.root_screen.screen_manager.current = 'controllers'
         open_nav_menu = lambda x: app.root_screen.ids._nav_drawer.set_state('open')
         app.root_screen.ids._top_app_bar.left_action_items = [['menu', open_nav_menu]]
-
